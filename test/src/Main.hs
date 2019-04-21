e72ba1c66089f7a79535b42eba38b904f08f2b4c
Philip Hazelden <philip.hazelden@gmail.com>
2019-04-21 22:05:09 +0100

    Improve testing.

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -3,8 +3,28 @@ module Main
     )
 where
 
+import           System.Exit                    ( exitFailure )
+import           System.IO                      ( readFile )
+
 import           A                              ( foo )
 
-printMe = ()
+-- Our testing strategy is: assume that this file was compiled with the correct
+-- flags, and that we're running in the root project directory. Then just check
+-- that the file produced in compilation is exactly what we expect.
 
-main = print ()
+myImports :: [(String, String)]
+myImports =
+    [("System.Exit", "exitFailure"), ("System.IO", "readFile"), ("A", "foo")]
+
+main = do
+    let expected = unlines $ map (\(a, b) -> a ++ "\t" ++ b) myImports
+    actual <- readFile "test/test-out/Mainmain"
+    if actual == expected
+        then putStrLn "OK" >> return ()
+        else do
+            putStrLn "Expected:"
+            putStrLn expected
+            putStrLn "\n---\n"
+            putStrLn "Actual:"
+            putStrLn actual
+            exitFailure
